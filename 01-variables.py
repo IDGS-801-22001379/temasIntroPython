55a4f2414b85506e5df9f4946c86cbf8ca824a37
IDGS-801-22001379 <yaelxdmt88xfire07@gmail.com>
2025-01-13 23:41:01 -0600

    primer commit

---
+++ 01-variables.py
@@ -0,0 +1,14 @@
+print('Ejemplo1 : ')
+
+num1= 5
+num2= 4
+res = num1 + num2
+print(res) 
+
+print('Dame dos numeros para sumarlos')
+print('')
+
+num1= int(input('INGRESA EL PRIMER NUMERO : '))
+num2= input('Ingresa el segundo numero : ')
+suma= num1 + int(num2)
+print('la suma {1} mas {2} es:  {0}' .format(suma, num1, num2))
